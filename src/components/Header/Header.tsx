6a2a7d0617f3999b9a51abc56cc46f034d92a9a0
Irene Tung <irene277299@gmail.com>
2022-05-01 23:20:43 +0800

    feat: support styled-components.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,30 @@
+import styled from 'styled-components';
+
 type Props = {
   className?: string;
 };
 
 export function Header({ className }: Props) {
-  return <div className={className}>header</div>;
+  return (
+    <HeaderContainer className={className}>
+      <HeaderLeft>header</HeaderLeft>
+    </HeaderContainer>
+  );
 }
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: fixed;
+  width: 100%;
+  padding: 10px 20px;
+  background-color: rgba(var(--color-secondary-default, 1));
+  color: white;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  flex: 0.3;
+  align-items: center;
+`;
