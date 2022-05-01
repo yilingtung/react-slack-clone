48911810e66f40141db25cd1ebfe8f53530fa1a2
Irene Tung <irene277299@gmail.com>
2022-05-01 19:37:22 +0800

    fix: remove useless files.

--- src/features/counterSlice.ts
+++ src/features/counterSlice.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState, AppThunk } from '../../app/store';
-import { fetchCount } from './counterAPI';
+import { RootState, AppThunk } from '../app/store';
 
 export interface CounterState {
   value: number;
@@ -20,9 +19,8 @@ const initialState: CounterState = {
 export const incrementAsync = createAsyncThunk(
   'counter/fetchCount',
   async (amount: number) => {
-    const response = await fetchCount(amount);
     // The value we return becomes the `fulfilled` action payload
-    return response.data;
+    return amount;
   }
 );
 
